31caa36f117068630313bf340d3ba9def889190f
Keith <krush@lostminions.org>
2016-03-20 16:53:09 -0700

    reorderablelist that looks pretty example

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelData : MonoBehaviour
+{
+    public List<MobWave> Waves = new List<MobWave>();
+
+    private struct WaveCreationParams
+    {
+        public MobWave.WaveType Type;
+        public string Path;
+    }
+}
